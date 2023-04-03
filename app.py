8c89c85a765e8a3d63635ed0c100b7c24866e752
CyberFlame <cyberflameu@gmail.com>
2023-04-03 14:36:54 +1200

    set up sentry properly

---
+++ app.py
@@ -0,0 +1,31 @@
+import sentry_sdk
+from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn = "https://eebca21dd9c9418cbfe83e7b8a0976de@o317122.ingest.sentry.io/4504873492480000",
+    integrations = [
+        FlaskIntegration(),
+    ],
+    send_default_pii = True,
+    traces_sample_rate = 1.0,
+    _experiments = {
+        "profiles_sample_rate": 1.0,
+    }
+)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    """
+    Displays a simple message to the user.
+    :return:
+    :rtype:
+    """
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
