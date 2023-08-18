f553227b8715ef8fa7f45bd2b6f272f385e09197
CyberFlame <cyberflameu@gmail.com>
2023-08-18 12:06:31 +1200

    provision repo for new assessment
    Signed-off-by: CyberFlame <cyberflameu@gmail.com>

--- app.py
+++ app.py
@@ -1,35 +1,28 @@
+import fastapi
 import sentry_sdk
-import flask
-from sentry_sdk.integrations.flask import FlaskIntegration
+
 import utils
 
 sentry_sdk.init(
-    dsn = "https://eebca21dd9c9418cbfe83e7b8a0976de@o317122.ingest.sentry.io/4504873492480000",
-    integrations = [
-        FlaskIntegration(),
-    ],
-    send_default_pii = True,
-    traces_sample_rate = 1.0,
-    _experiments = {
+    dsn="https://eebca21dd9c9418cbfe83e7b8a0976de@o317122.ingest.sentry.io/4504873492480000",
+    send_default_pii=True,
+    traces_sample_rate=1.0,
+    _experiments={
         "profiles_sample_rate": 1.0,
-    }
+    },
 )
 
-app = flask.Flask(__name__)
+app = fastapi.FastAPI()
 
 
 # db = utils.Database() TODO: setup database tables and re-jig the spreadsheet layout
 
 
-@app.route('/')
-def hello_world():  # put application's code here
+@app.get("/")
+async def hello_world():  # put application's code here
     """
     Displays the homepage to the user
     :return:
     :rtype:
     """
-    return flask.render_template('home.html')
-
-
-if __name__ == '__main__':
-    app.run(debug = True)
+    return flask.render_template("home.html")
