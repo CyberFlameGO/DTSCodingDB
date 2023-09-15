f70fe659a920e53afc4f5854f3acc876f457cadf
CyberFlame <cyberflameu@gmail.com>
2023-09-15 13:57:27 +1200

    work thus far
    Signed-off-by: CyberFlame <cyberflameu@gmail.com>

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,9 @@
 Base class
 """
 from datetime import datetime
+from typing import FrozenSet
 
-from sqlalchemy import DateTime, Integer, String
+from sqlalchemy import DateTime, ForeignKey, Integer, String
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, Relationship, mapped_column, relationship
 
@@ -22,8 +23,6 @@ class Games(Base):
     id: Mapped[int] = mapped_column(autoincrement=True, nullable=False, unique=True, primary_key=True)
     name: Mapped[str] = mapped_column()
     description: Mapped[str] = mapped_column()
-
-    matches: Mapped["Matches"] = relationship(back_populates="game")
 
 
 class Users(Base):
@@ -46,8 +45,25 @@ class Matches(Base):
 
     id: Mapped[int] = mapped_column(autoincrement=True, nullable=False, unique=True, primary_key=True)
     game_type: Mapped[str] = mapped_column()
-    winner: Mapped[str] = mapped_column()
-    loser: Mapped[str] = mapped_column()
     played_at: Mapped[DateTime] = mapped_column(default=datetime.utcnow)
     creator: Mapped[str] = mapped_column()
     created_at: Mapped[DateTime] = mapped_column(default=datetime.utcnow)
+    players: Mapped[FrozenSet["MatchPlayers"]] = relationship(back_populates="matches")
+    results: Mapped[FrozenSet["MatchResult"]] = relationship(back_populates="matches")
+
+
+class MatchPlayers(Base):
+    __tablename__: str = "matchplayers"
+
+    id: Mapped[int] = mapped_column(autoincrement=True, nullable=False, unique=True, primary_key=True)
+    match_id: Mapped[int] = mapped_column(ForeignKey("matches.id"))
+    player_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+
+class MatchResult(Base):
+    __tablename__: str = "matchresults"
+
+    id: Mapped[int] = mapped_column(autoincrement=True, nullable=False, unique=True, primary_key=True)
+    match_id: Mapped[int] = mapped_column(ForeignKey("matches.id"))
+    won: Mapped[str] = mapped_column()
+    lost: Mapped[str] = mapped_column()
